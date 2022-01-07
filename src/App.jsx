b65da09bfd174b0743199ce22bed1616b80661fc
unknown <adeoyesulaimon@yahoo.com>
2022-01-07 21:58:31 +0100

    nasa images

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import React from "react"; 
+import Imagecontainer from "./Imagecomponent/Imagecontainer";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Spacestargram</h1>
+      <Imagecontainer/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Imagecomponent/Imagecontainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Imagecontainer() {
+    return (
+        <div>
+            <img></img>
+            <title></title>
+        </div>
+    )
+}
+
+export default Imagecontainer
