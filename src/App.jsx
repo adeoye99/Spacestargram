68e672890255de75befe852a265639d88cd7329f
adeoyesulaimon@yahoo.com <adeoyesulaimon@yahoo.com>
2022-01-13 16:03:00 +0100

    design changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,47 @@
 import React,{useState,useEffect} from "react"; 
 import Imagecontainer from "./Imagecomponent/Imagecontainer";
 import axios from "axios";
-import Checkbox from '@mui/material/Checkbox';
-import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
-import Favorite from '@mui/icons-material/Favorite';
-import { pink } from '@mui/material/colors';
 import ClipLoader from "react-spinners/ClipLoader";
 import "./App.css";
+import { RequestQuoteRounded } from "@mui/icons-material";
 
-const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
+
+
 function App() {
   const [ Infos , setInfos] = useState([]);
   const [errors,setErrors] = useState();
   const [isLoading,setIsloading] = useState(false)
 
    useEffect(()=> {
-     setIsloading(true);
+        setIsloading(true)
         axios
         .get(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=cgFx60Cd2ijjyPngdFKcKGO3tjDYKaYgwuNvkMzQ`)
         .then((response) =>{
           const infos = response.data.photos;
-          setInfos(infos)
-          setIsloading(false);
+          setInfos(infos);
+          setIsloading(false)
             })
          
         },[]);
-
-return isLoading ?( <h4>Loading...</h4>):
-(
+        function createContainer(Info){
+         return(
+          <Imagecontainer
+             img = {Info.img_src}
+             title={Info.camera.full_name}
+             name = {Info.rover.name}
+             date = {Info.earth_date}
+          />
+         )
+        
+        }
+return isLoading ?(<h3>Loading.....</h3>):(
     <div className="App">
-         <h1 className ="app__logo">Spacestargram</h1>
-          {Infos.map((info,key)=>{
-            return (
-           <div key={info.id}>
-               <img className = "app__image"src ={info.img_src} alt = {info.title}></img>
-                 <p className = "app__fullname">{info.camera.full_name} - {info.rover.name}</p>
-                 <div className = "app__favorite">
-                 <p className = "app__earthdate">{info.earth_date}</p>
-                 <Checkbox 
-                   {...label} 
-                    icon={<FavoriteBorder />} 
-                    checkedIcon={<Favorite />} 
-                    sx={{
-                      color: pink[800],
-                    '&.Mui-checked': {
-                    color: pink[600],
-                     },
-                }}
-                 />
-                 </div>
-
-            </div>)
-         })}
-          </div>
- );
- 
+        <h1 className ="app__logo">Spacestargram</h1>
+        <p className = "app__subtopic">Brought to you from the NASA's image api</p>
+         {Infos.map(createContainer)}
+      
+     </div>
+ )
 }
-
+ 
 export default App;
--- src/Imagecomponent/Imagecontainer.jsx
+++ src/Imagecomponent/Imagecontainer.jsx
@@ -1,10 +1,32 @@
 import React ,{useEffect,useState} from 'react';
 import axios from "axios";
-
-function Imagecontainer() {
+import Checkbox from '@mui/material/Checkbox';
+import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
+import Favorite from '@mui/icons-material/Favorite';
+import { pink } from '@mui/material/colors';
+import ClipLoader from "react-spinners/ClipLoader";
+import "./Imagecontainer.css"
+const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
+function Imagecontainer(props) {
     return (
-        <div>
-            <title></title>
+        <div className = "imagecontainer">
+            <img className="imagecontainer__image" src = {props.img}></img>
+            <div className = "name">
+              <p className="imagecontainer__name">{props.name} {"-"} {props.title}</p>
+            </div>
+            
+            <p className="imagecontainer__date">{props.date}</p>
+            <Checkbox 
+                   {...label} 
+                    icon={<FavoriteBorder />} 
+                    checkedIcon={<Favorite />} 
+                    sx={{
+                     color: pink[800],
+                    '&.Mui-checked': {
+                    color: pink[600],
+                     },
+                 }}
+                 />              
         </div>
     )
 }
