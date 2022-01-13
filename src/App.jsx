545717b3e159d6774b5e344bbd228a2472409893
adeoyesulaimon@yahoo.com <adeoyesulaimon@yahoo.com>
2022-01-13 11:19:58 +0100

    added some styling

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,7 @@ import Checkbox from '@mui/material/Checkbox';
 import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
 import Favorite from '@mui/icons-material/Favorite';
 import { pink } from '@mui/material/colors';
-
-
+import ClipLoader from "react-spinners/ClipLoader";
 import "./App.css";
 
 const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
@@ -27,16 +26,17 @@ function App() {
          
         },[]);
 
-return isLoading ?(<h4>Loading.....</h4>):
+return isLoading ?( <h4>Loading...</h4>):
 (
     <div className="App">
-         <h1>Spacestargram</h1>
+         <h1 className ="app__logo">Spacestargram</h1>
           {Infos.map((info,key)=>{
             return (
            <div key={info.id}>
                <img className = "app__image"src ={info.img_src} alt = {info.title}></img>
-                 <p>{info.camera.full_name} - {info.rover.name}</p>
-                 <p>{info.earth_date}</p>
+                 <p className = "app__fullname">{info.camera.full_name} - {info.rover.name}</p>
+                 <div className = "app__favorite">
+                 <p className = "app__earthdate">{info.earth_date}</p>
                  <Checkbox 
                    {...label} 
                     icon={<FavoriteBorder />} 
@@ -48,6 +48,8 @@ return isLoading ?(<h4>Loading.....</h4>):
                      },
                 }}
                  />
+                 </div>
+
             </div>)
          })}
           </div>
